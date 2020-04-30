8c47f82f7f2d046672b72e25b70ff2277f26e2e7
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-30 23:35:27 +0600

    Created post and delete method

--- Router.php
+++ Router.php
@@ -4,6 +4,8 @@ namespace App\Routing;
 
 class Router
 {
+    private static $noMatch = true;
+
     private static function getUrl()
     {
         return $_SERVER['REQUEST_URI'];
@@ -12,23 +14,56 @@ class Router
     private static function getMatches($pattern)
     {
         $url = self::getUrl();
-        if(preg_match($pattern, $url, $matches))
-        {
+        if (preg_match($pattern, $url, $matches)) {
             return $matches;
         }
         return false;
     }
 
-   public static function get($pattern, $callback)
+    private static function process($pattern, $callback)
     {
-        $pattern = "~^{$pattern}$~";
+        $pattern = "~^{$pattern}/?$~";
         $params = self::getMatches($pattern);
 
-        if($params){
-            if(is_callable($callback)){
+        if ($params) {
+            if (is_callable($callback)) {
+                self::$noMatch = false;
                 $functionArguments = array_slice($params, 1);
                 $callback(...$functionArguments);
             }
+        }
+    }
+
+    public static function get($pattern, $callback)
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+            return;
+        }
+
+        self::process($pattern,$callback);
+    }
+
+    public static function post($pattern, $callback)
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+            return;
+        }
+        self::process($pattern,$callback);
+    }
+
+    public static function delete($pattern, $callback)
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'DELETE') {
+            return;
+        }
+        
+        self::process($pattern,$callback);
+    }
+
+    public static function cleanup()
+    {
+        if (self::$noMatch) {
+            echo "404! No Routes found";
         }
     }
 }
